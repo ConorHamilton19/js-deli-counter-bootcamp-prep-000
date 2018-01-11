7668cb2f857500adaf194fe047ca72ecc8954bdb
Conor Hamilton <conhamil19@gmail.com>
2018-01-11 05:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing() {
+if (katzDeliLine.length>0)
   return katzDeliLine[0]
+  karzDeliLine.shift()
+  
+else
+  return "There is nobody waiting to be served!"
 }
