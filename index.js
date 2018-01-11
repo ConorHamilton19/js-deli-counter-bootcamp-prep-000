31596bfd2dd550c307a0c3bdc1898c60a76e17ec
Conor Hamilton <conhamil19@gmail.com>
2018-01-11 05:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeli = []
 
 function takeANumber(katzDeliLine, name) {
+  for (var i = 1; i<katzDeliLine.length; i++) {
   return `Welcome, ${name}. You are number ${katzDeliLine} in line.`
-  
+  }
 }
